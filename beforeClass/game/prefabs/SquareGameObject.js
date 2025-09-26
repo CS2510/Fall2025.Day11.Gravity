0bfa559a8b7bc915a76969b15fedc5bbeb226356
bricks <bricks@unomaha.edu>
2025-09-26 16:03:42 -0500

    Remove old beforeClass folder

--- beforeClass/game/prefabs/SquareGameObject.js
+++
@@ -1,8 +0,0 @@
-class SquareGameObject extends GameObject{
-    constructor(){
-        super("Player Game Object")
-        this.addComponent(new SquareController())
-        this.addComponent(new Polygon(), {fillStyle:"red"})
-        this.transform.scale = new Vector2(20, 20)
-    }
-}
--- beforeClass/game/prefabs/TriangleGameObject.js
+++
@@ -1,8 +0,0 @@
-class TriangleGameObject extends GameObject{
-    constructor(){
-        super('Enemy Game Object')
-        this.addComponent(new TriangleController())
-        this.addComponent(new Polygon(), {fillStyle: "blue"})
-        this.transform.scale = new Vector2(15, 15)
-    }
-}
